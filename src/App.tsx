55b24e0d7c59f9dbd2ada9b7965f0c3e2c4f8fc4
visidhreachoun <visidhreachoun@gmail.com>
2023-09-14 09:58:35 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App = () => {
   const [state, setState] = useState("1");
 
   const [width, setWidth] = useState(window.innerWidth);
-  const breakPoint = 570;
-
+  const breakPoint = 1000;
+  const hideSearchBar = 380;
   useEffect(() => {
     const handleWindowResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleWindowResize);
@@ -35,68 +35,7 @@ const App = () => {
               d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
             ></path>
           </svg>
-          <div class="ml-1 flex items-center mt-2 star_rating_display">
-            <svg
-              class="text-[#ffd25c] h-5 w-5 flex-shrink-0"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              class="text-[#ffd25c] h-5 w-5 flex-shrink-0"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              class="text-[#ffd25c] h-5 w-5 flex-shrink-0"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              class="text-[#ffd25c] h-5 w-5 flex-shrink-0"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              class="text-[#ffd25c] h-5 w-5 flex-shrink-0"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </div>
+
           <span className="buttonText" style={{ marginLeft: "8px" }}>
             {item.cate}
           </span>
@@ -187,39 +126,41 @@ const App = () => {
             width={115}
           />
 
-          <div
-            className="searchBar"
-            style={{
-              borderColor: focused ? "rgb(58 186 204)" : "#f0efef",
-            }}
-          >
-            <svg
-              className="searchIcon"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
+          {width > hideSearchBar && (
+            <div
+              className="searchBar"
+              style={{
+                borderColor: focused ? "rgb(58 186 204)" : "#f0efef",
+              }}
             >
-              <path
-                fill-rule="evenodd"
-                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+              <svg
+                className="searchIcon"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
 
-            <input
-              type="search"
-              className="textBox"
-              placeholder="Search"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-              onFocus={() => setFocused(true)}
-              onBlur={() => setFocused(false)}
-            />
-          </div>
+              <input
+                type="search"
+                className="textBox"
+                placeholder="Search"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                onFocus={() => setFocused(true)}
+                onBlur={() => setFocused(false)}
+              />
+            </div>
+          )}
         </div>
       </div>
       <div className="body">
-        <div style={{ paddingBottom: "50px" }}>
+        <div className="flex items-center">
           <span className="topText">TOP</span>
           <svg
             width="7"
@@ -273,7 +214,185 @@ const App = () => {
             {data.map((item) => renderEachCard(item))}
           </div>
         </div>
+        {/* tail wind */}
+        <div className="paginationContainer">
+          <span class="relative z-0 inline-flex">
+            <span aria-disabled="true" aria-label="pagination.previous">
+              <span
+                class=" bg-[#22acbf] relative inline-flex items-center mr-4 px-2 py-2 text-sm font-medium text-white bg-gray-line cursor-default rounded-full leading-5 "
+                aria-hidden="true"
+              >
+                <svg class="w-5 h-5" fill="white" viewBox="0 0 20 20">
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+            </span>
+
+            <span aria-current="page">
+              <span class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm border-b-2 border-primary font-medium text-gray-500 bg-white cursor-default leading-5">
+                1
+              </span>
+            </span>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 2"
+            >
+              2
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 3"
+            >
+              3
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 4"
+            >
+              4
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 5"
+            >
+              5
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 6"
+            >
+              6
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 7"
+            >
+              7
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 8"
+            >
+              8
+            </a>
+            <a
+              href=""
+              class="relative inline-flex items-center mx-4 px-2 py-2 -ml-px text-sm font-medium text-gray-text border-b-2 border-gray-line bg-white leading-5 md:hover:opacity-75 md:transition-opacity md:ease-in"
+              aria-label="Go to page 9"
+            >
+              9
+            </a>
+
+            <a
+              href=""
+              rel="next"
+              class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-white bg-primary rounded-full leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+              aria-label="pagination.next"
+            >
+              <span
+                class=" bg-[#22acbf] relative inline-flex items-center mr-4 px-2 py-2 text-sm font-medium text-white bg-gray-line cursor-default rounded-full leading-5 "
+                aria-hidden="true"
+              >
+                <svg class="w-5 h-5" fill="white" viewBox="0 0 20 20">
+                  <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+            </a>
+          </span>
+        </div>
       </div>
+      <footer
+        aria-labelledby="footer-heading"
+        class=" lg:mt-28 bg-gray-100 text-black max-w-full"
+      >
+        <h2 id="footer-heading" class="sr-only">
+          Footer
+        </h2>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div class="pt-10 pb-20 xl:grid xl:grid-cols-3 xl:gap-8">
+            <div class="mb-8">
+              <img
+                width="1903"
+                height="781"
+                class="py-2 h-auto w-[124px]"
+                src="https://appliv-gai-production.s3.ap-northeast-1.amazonaws.com/images/logo.png"
+                alt="AI比較ナビ"
+              />
+            </div>
+            <div class="grid grid-cols-2 gap-8 col-span-2">
+              <div>
+                <ul role="list" class="space-y-6">
+                  <li class="text-sm">
+                    <a
+                      href="http://nyle.co.jp/"
+                      target="_blank"
+                      class="md:hover:opacity-75 md:transition-opacity md:ease-in"
+                    >
+                      Operation Company
+                    </a>
+                  </li>
+
+                  <li class="text-sm">
+                    <a
+                      href=""
+                      target="_blank"
+                      class="md:hover:opacity-75 md:transition-opacity md:ease-in"
+                    >
+                      Inquiry
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <ul role="list" class="space-y-6">
+                  <li class="text-sm">
+                    <a
+                      href=""
+                      target="_blank"
+                      class="md:hover:opacity-75 md:transition-opacity md:ease-in"
+                    >
+                      Terms of Services
+                    </a>
+                  </li>
+
+                  <li class=" text-sm">
+                    <a
+                      href=""
+                      target="_blank"
+                      class="md:hover:opacity-75 md:transition-opacity md:ease-in"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="border-t border-gray-line py-10">
+            <p className="text-sm  text-gray-200">Copyright © Nyle Inc.</p>
+            <p className="mt-3 text-xs text-gray-400">
+              Android は Google Inc. の商標です。
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
